db1e8668f842093b88f8edc760b4331652ea360d
Dykizz <phungphutai2005@gmail.com>
2026-01-30 10:39:47 +0700

    Sửa lại các sbd hợp lệ đều có học sinh

--- mssql/setup.sql
+++ mssql/setup.sql
@@ -1,4 +1,4 @@
--- Reset và setup lại toàn bộ 3 database với GAPS (SBD bị trống) để test trường hợp không tìm thấy thí sinh
+-- Reset và setup lại toàn bộ 3 database với đầy đủ thí sinh (1-1000)
 -- ==========================================================
 
 -- 1. Setup THISINH_BAC
@@ -18,19 +18,15 @@ GO
 DECLARE @i INT = 1;
 WHILE @i <= 300
 BEGIN
-    -- Bỏ qua SBD 10, 50, 100 để test trường hợp không có dữ liệu
-    IF @i NOT IN (10, 50, 100)
-    BEGIN
-        INSERT INTO ThiSinh VALUES (
-            @i, 
-            CONCAT(
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Nguyễn', N'Trần', N'Lê', N'Phạm', N'Hoàng', N'Phan', N'Vũ', N'Đặng', N'Bùi', N'Đỗ'), ' ', 
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Văn', N'Thị', N'Minh', N'Thanh', N'Đức', N'Ngọc', N'Quang', N'Hữu', N'Anh', N'Kim'), ' ', 
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'An', N'Bình', N'Cường', N'Dũng', N'Giang', N'Hương', N'Khánh', N'Linh', N'Nam', N'Sơn')
-            ),
-            N'Miền Bắc', ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2)
-        );
-    END
+    INSERT INTO ThiSinh VALUES (
+        @i, 
+        CONCAT(
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Nguyễn', N'Trần', N'Lê', N'Phạm', N'Hoàng', N'Phan', N'Vũ', N'Đặng', N'Bùi', N'Đỗ'), ' ', 
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Văn', N'Thị', N'Minh', N'Thanh', N'Đức', N'Ngọc', N'Quang', N'Hữu', N'Anh', N'Kim'), ' ', 
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'An', N'Bình', N'Cường', N'Dũng', N'Giang', N'Hương', N'Khánh', N'Linh', N'Nam', N'Sơn')
+        ),
+        N'Miền Bắc', ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2)
+    );
     SET @i = @i + 1;
 END
 GO
@@ -52,19 +48,15 @@ GO
 DECLARE @k INT = 301;
 WHILE @k <= 600
 BEGIN
-    -- Bỏ qua SBD 350, 450, 550
-    IF @k NOT IN (350, 450, 550)
-    BEGIN
-        INSERT INTO ThiSinh VALUES (
-            @k, 
-            CONCAT(
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Phan', N'Võ', N'Trương', N'Bùi', N'Ngô', N'Hồ', N'Dương', N'Đặng', N'Lý', N'Mai'), ' ', 
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Hữu', N'Thanh', N'Ngọc', N'Quang', N'Anh', N'Minh', N'Tuyết', N'Xuân', N'Tuấn', N'Phương'), ' ', 
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Thành', N'Hùng', N'Kiên', N'Long', N'Quân', N'Triết', N'Việt', N'Yến', N'Hà', N'Tuệ')
-            ),
-            N'Miền Trung', ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2)
-        );
-    END
+    INSERT INTO ThiSinh VALUES (
+        @k, 
+        CONCAT(
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Phan', N'Võ', N'Trương', N'Bùi', N'Ngô', N'Hồ', N'Dương', N'Đặng', N'Lý', N'Mai'), ' ', 
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Hữu', N'Thanh', N'Ngọc', N'Quang', N'Anh', N'Minh', N'Tuyết', N'Xuân', N'Tuấn', N'Phương'), ' ', 
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Thành', N'Hùng', N'Kiên', N'Long', N'Quân', N'Triết', N'Việt', N'Yến', N'Hà', N'Tuệ')
+        ),
+        N'Miền Trung', ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2)
+    );
     SET @k = @k + 1;
 END
 GO
@@ -86,19 +78,15 @@ GO
 DECLARE @j INT = 601;
 WHILE @j <= 1000
 BEGIN
-    -- Bỏ qua SBD 650, 750, 850
-    IF @j NOT IN (650, 750, 850)
-    BEGIN
-        INSERT INTO ThiSinh VALUES (
-            @j, 
-            CONCAT(
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Lý', N'Huỳnh', N'Lâm', N'Dương', N'Mai', N'Vương', N'Trịnh', N'Đoàn', N'Tô', N'Cao'), ' ', 
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Thành', N'Kim', N'Minh', N'Quốc', N'Trọng', N'Khải', N'Duy', N'Thảo', N'Trang', N'Oanh'), ' ', 
-                CHOOSE(CAST(RAND()*10+1 AS INT), N'Phát', N'Đạt', N'Thịnh', N'Vượng', N'Khang', N'Phúc', N'Lộc', N'Thọ', N'Tâm', N'Hào')
-            ),
-            N'Miền Nam', ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2)
-        );
-    END
+    INSERT INTO ThiSinh VALUES (
+        @j, 
+        CONCAT(
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Lý', N'Huỳnh', N'Lâm', N'Dương', N'Mai', N'Vương', N'Trịnh', N'Đoàn', N'Tô', N'Cao'), ' ', 
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Thành', N'Kim', N'Minh', N'Quốc', N'Trọng', N'Khải', N'Duy', N'Thảo', N'Trang', N'Oanh'), ' ', 
+            CHOOSE(CAST(RAND()*10+1 AS INT), N'Phát', N'Đạt', N'Thịnh', N'Vượng', N'Khang', N'Phúc', N'Lộc', N'Thọ', N'Tâm', N'Hào')
+        ),
+        N'Miền Nam', ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2), ROUND(RAND()*6+4,2)
+    );
     SET @j = @j + 1;
 END
 GO
